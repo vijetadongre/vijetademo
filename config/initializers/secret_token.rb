1098a1c0cef4af906cc10b88afe2215aa5ad3731
vijeta <vijetalfs@gmail.com>
2013-05-08 19:15:11 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vijetademo::Application.config.secret_token = '76f5779b12b101fd2175e5def50073fd2553a140a6335b3e89abb485785a46c93cf0a1b74778760165551c566a3c86b92d721eb2ce855152606b975d9f4b62e7'
